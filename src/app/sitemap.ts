29376c76b9265cc01ae7b91fbfccd5f948ef6bfc
sebastien Amafou <sebastien.amafou@gmail.com>
2026-03-17 21:43:59 +0100

    fix: compliance id lease route

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next';
 import { prisma } from "@/lib/prisma"; // 👈 Vérifiez que ce chemin vers votre instance Prisma est correct
-
+export const dynamic = 'force-dynamic';
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://www.immofacile.ci';
 
